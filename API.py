b17f7e15b78b0da6b698689d9f8fc16ee600caa5
dorismugure <dorismugure2012@gmail.com>
2017-03-08 18:47:01 +0300

    API command line

---
+++ API.py
@@ -0,0 +1,25 @@
+import pprint
+
+import sys
+
+from apiclient.discovery import build
+
+api_key = sys.argv[1]
+
+service = build('books', 'v1', developerKey=api_key)
+
+request = service.volumes().list(source='public', q='android')
+
+response = request.execute()
+
+pprint.pprint(response)
+
+print ('Found %d books:' % len(response['list']))
+
+for book in response.get('list', []):
+
+  print ('Title: %s, Authors: %s' % (
+
+    book['volumeInfo']['title'],
+
+    book['volumeInfo']['authors']))
